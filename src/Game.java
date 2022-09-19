6319ab19ffdcd618d299f2f93116ffa23604e3d6
William Zinkann <wzinkann@gmail.com>
2022-09-19 12:47:26 -0400

    Pre class work 09/19

--- src/Game.java
+++ src/Game.java
@@ -9,8 +9,11 @@ public class Game {
 	private final int LOSER_ROLL = 1;
 	
 	public Game(){
-		Player player1 = new GUIPlayer();
-		Player player2 = new ComputerPlayer();
+//		Player player1 = new GUIPlayer();
+//		Player player2 = new ComputerPlayer();
+//		No need to initialize new player objects, already created above
+		player1 = new GUIPlayer();
+		player2 = new ComputerPlayer();
 		die = new Random();
 		spinner = new Spinner();
 	}
